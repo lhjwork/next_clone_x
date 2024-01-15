bc151b767846e4db66a8ca4247adf95fe744327f
lhjwork <jin.come.up.business@gmail.com>
2024-01-15 21:19:21 +0900

    default.tsx applied so beforeLogin's layout.tsx is button click monent about 로그인, modal일어남

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -14,3 +14,6 @@ export default function Layout({ children, modal }: Props) {
     </div>
   );
 }
+
+// 주소가 localhost:3000일 때 children -> page.tsx, modal -> @modal/default.tsx
+// 주소가 localhost:3000/i/flow/login 때는 children-> i/flow/login/page.tsx, modal->@modal/i/flow/login/page.tsx
